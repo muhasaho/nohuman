488725faf847eb3e359f0a88ee68d373b9202549
Muhammed Saho <muhasaho@gmail.com>
2015-05-19 22:27:20 -0500

    set panel style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,6 +8,20 @@
  * Controller of the nohumanApp
  */
 angular.module('nohumanApp')
+.filter('toArray', function () {
+    'use strict';
+
+    return function (obj) {
+        if (!(obj instanceof Object)) {
+            return obj;
+        }
+
+        return Object.keys(obj).map(function (key) {
+            return Object.defineProperty(obj[key], '$key', {__proto__: null, value: key});
+        });
+    };
+})
+
   .controller('MainCtrl', function ($scope, $firebaseArray) {
     var ref = new Firebase("https://nohuman.firebaseio.com/");
     
